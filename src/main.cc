786d502b345b547378048ab662520bf1631675c3
VC Science Labs <travis_chamness1@my.vcccd.edu>
2018-09-27 09:35:10 -0700

    9/27 Is passing 10 of 20

--- src/main.cc
+++ src/main.cc
@@ -1,12 +1,26 @@
 #include<iostream>
+#include "gtest/gtest.h"
 #include"windchillindex.h"
+#include <climits>
+#include <cmath>
+using edu::vcccd::vc::csv13::computeWindChillIndex;
+using std::cout;
+using std::endl;
+using std::cin;
 
-int main(int argc, char *argv[]) {
-	double t = 9.9;
-	double v = 11;
+int main(int64_t argc, char *argv[]) {
 	
+	double t;
+	double v;
 
-
+	cout << "Please enter your local temperature(as long as its less than 10 celcius): ";
+	cin >> t;
+	cout << "/n Please enter your local wind Velocity in Meters/Second: ";
+	cin >> v;
+	
+	
+	double index = computeWindChillIndex(t, v);
+	cout << "index: " << index << endl;
 
 	return 0;
 }
