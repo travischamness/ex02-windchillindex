b54a5378af7afd89c522f30109f0c4330a7ded9d
Travis <travis_chamness1@my.vcccd.edu>
2018-10-03 10:09:33 -0700

    10/3 I KNOW THIS MUST WORK WTF

--- src/main.cc
+++ src/main.cc
@@ -1,6 +1,4 @@
 #include<iostream>
-#include<string>
-#include "gtest/gtest.h"
 #include"windchillindex.h"
 #include <cmath>
 using std::cout;
@@ -10,17 +8,7 @@ using edu::vcccd::vc::csv13::computeWindChillIndex;
 
 int main(int argc, char *argv[]) {
 	
-	double t;
-	double v;
-
-	cout << "Please enter your local temperature(as long as its less than 10 celcius): ";
-	cin >> t;
-	cout << "\n Please enter your local wind Velocity in Meters/Second: ";
-	cin >> v;
-
-
-	double index = computeWindChillIndex(t, v);
-	cout << "index: " << index << endl;
+	cout <<computeWindChillIndex(5,11);
 
 	return 0;
 }
